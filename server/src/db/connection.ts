dd79fcaa95fdbe77ed06c9c343c0cc30f1d107cf
Oluwatobi <krisella74@gmail.com>
2023-03-01 07:31:12 +0100

    load env mongo connection url

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
+const mongoURL = process.env.MONGO_URL ?? "mongodb://127.0.0.1:27017";
+
+const connectionURL = `${mongoURL}/tictactoe`;
+
 async function connectDB() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/tictactoe");
+  await mongoose.connect(connectionURL);
 }
 
 connectDB();
